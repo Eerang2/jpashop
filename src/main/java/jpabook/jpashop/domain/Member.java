3d25b7127e1d9f02ff24f1f6fd2c59d7fb03edc7
Eerang2 <117508537+Eerang2@users.noreply.github.com>
2023-04-02 16:06:15 +0900

    홈화면에 있는 회원 등록, 목록 상품 등록,목록,수정 화면 연결코드 연습

--- src/main/java/jpabook/jpashop/domain/Member.java
+++ src/main/java/jpabook/jpashop/domain/Member.java
@@ -10,13 +10,15 @@ import java.util.List;
 @Entity
 @Getter @Setter
 public class Member {
-@Id @GeneratedValue
-@Column(name = "member_id")
+    @Id @GeneratedValue
+    @Column(name = "member_id")
     private Long id;
 
     private String name;
+
     @Embedded
     private Address address;
+
     @OneToMany(mappedBy = "member")
     private List<Order> orders = new ArrayList<>();
 }
--- src/main/java/jpabook/jpashop/domain/Order.java
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -8,6 +8,8 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
+import static javax.persistence.FetchType.LAZY;
+
 @Entity
 @Table(name = "orders")
 @Getter @Setter
@@ -16,13 +18,14 @@ public class Order {
     @Column(name = "oders_id")
     private Long id;
 
-    @ManyToOne
+    @ManyToOne(fetch = LAZY)
     @JoinColumn(name = "member_id")
     private Member member;
 
-    @OneToMany(mappedBy = "order")
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
     private List<OrderItem> orderItems = new ArrayList<>();
-    @OneToOne
+
+    @OneToOne(fetch = LAZY, cascade = CascadeType.ALL)
     @JoinColumn(name = "delivery_id")
     private Delivery delivery;
 
@@ -31,7 +34,7 @@ public class Order {
     private OrderStatus status;   //주문상태 [ORDER, CANCLE]
 
     //==연관관계 메소드==//
-    public void setMember() {
+    public void setMember(Member member) {
         this.member = member;
         member.getOrders().add(this);
     }
@@ -41,7 +44,7 @@ public class Order {
         orderItem.setOrder(this);
     }
 
-    public void setDelivery() {
+    public void setDelivery(Delivery delivery) {
         this.delivery = delivery;
         delivery.setOrder(this);
     }

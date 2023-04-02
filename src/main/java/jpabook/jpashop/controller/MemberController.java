23ba2c0e2c92865059f74db204b94e974ee1b879
Eerang2 <117508537+Eerang2@users.noreply.github.com>
2023-04-02 23:14:15 +0900

    최종 완성 코드

--- src/main/java/jpabook/jpashop/controller/MemberController.java
+++ src/main/java/jpabook/jpashop/controller/MemberController.java
@@ -7,11 +7,12 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import javax.validation.Valid;
 import java.util.List;
+
 
 @Controller
 @RequiredArgsConstructor
@@ -26,7 +27,7 @@ public class MemberController {
     }
 
     @PostMapping("/members/new")
-    public String create(@Validated MemberForm form, BindingResult result) {
+    public String create(@Valid MemberForm form, BindingResult result) {
 
         if (result.hasErrors()) {
             return "members/creatMemberForm";
--- src/main/java/jpabook/jpashop/controller/MemberForm.java
+++ src/main/java/jpabook/jpashop/controller/MemberForm.java
@@ -3,6 +3,8 @@ package jpabook.jpashop.controller;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.validation.constraints.NotEmpty;
+
 @Getter @Setter
 public class MemberForm {
     @NotEmpty(message = "회원이름은 필수 입니다.")
